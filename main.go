7251825b304e25389b46f98731e6ad38e92a386f
endo\endo2 <sirayusan3636@gmail.com>
2024-01-27 03:19:22 +0900

    #2 アーキテクチャ構成

--- main.go
+++ main.go
@@ -1,41 +1,20 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
 	"os"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"business/driver"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env not found")
 	}
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
-}
-
-// Simple implementation of an integer minimum
-// Adapted from: https://gobyexample.com/testing-and-benchmarking
-func IntMin(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	log.Println("Server running...")
+	driver.Serve(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
