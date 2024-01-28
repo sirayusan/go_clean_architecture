ef54d89dabd5ca9cd00c4263f7de6850085a4d64
endo\endo2 <sirayusan3636@gmail.com>
2024-01-28 17:56:52 +0900

    デバッガ導入

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/labstack/echo/v4"
+	"net/http"
 	"os"
-
-	"business/driver"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(".env not found")
-	}
+	// Echoインスタンスを作成
+	e := echo.New()
+	go
 
-	log.Println("Server running...")
-	driver.Serve(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	// ルートルートに対するハンドラを登録
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Echo!")
+	})
+
+	// サーバーを8080ポートで起動
+	if err := e.Start(os.ExpandEnv(":${GO_PORT}")); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
