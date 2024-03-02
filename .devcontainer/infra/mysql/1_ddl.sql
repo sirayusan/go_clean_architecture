ea199680e4e89f731c1eefcada9cc8ea288f1ca5
endo\endo2 <sirayusan3636@gmail.com>
2024-03-02 19:01:08 +0900

    test

--- .devcontainer/infra/mysql/1_ddl.sql
+++ .devcontainer/infra/mysql/1_ddl.sql
@@ -1,9 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS `development` DEFAULT CHARACTER SET utf8mb4 ;
 CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 ;
 
+-- 'user'@'%' が未作成の場合、ユーザーを作成
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
+-- 'user'@'%' に対して `test` データベースの全権限を付与
 GRANT ALL PRIVILEGES ON `test`.* TO 'user'@'%';
 FLUSH PRIVILEGES;
-SET CHARSET utf8mb4;
+
+-- セッションの文字セットをutf8mb4に設定
+SET NAMES 'utf8mb4';
+
 --
 -- -- -----------------------------------------------------
 -- -- Table `clean_architecture`.`users`
